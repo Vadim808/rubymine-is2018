e6b64a66489b35cc6368e0dd50a0362eb74b3d74
Vadim808 <vch.98@mail.ru>
2018-04-26 00:26:27 +0300

    Тесты на логические выражения.

---
+++ test4.py
@@ -0,0 +1,22 @@
+if 3 > 4 and 5 < 10:
+    pass
+if 3 < 4 and 5 < 10:
+    pass
+if 3 > 4 and 4 > 10:
+    pass
+
+if 3 > 4 or 5 < 10:
+    pass
+if 3 < 4 or 5 < 10:
+    pass
+if 3 > 4 or 4 > 10:
+    pass
+
+if not(3 > 4 or 5 < 10):
+    pass
+if not(3 < 4 and 5 < 10):
+    pass
+if not(3 > 4) or 5 > 10:
+    pass
+
+
